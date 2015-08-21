f5c180f51f7092c04fe53243e407defc77e55298
Teofilo Sibileau <teo.sibileau@gmail.com>
2015-08-21 19:56:40 -0300

    draft for receiver

---
+++ receiver.js
@@ -0,0 +1,10 @@
+var appid = 'D8AEA46D';
+var ns = ['com.chartcast.demo'];
+
+var receiver = new cast.receiver.Receiver(appid, ns);
+
+receiver.start();
+
+window.addEventListener('load', function() {
+  $("#message").text("Loaded!");
+});
